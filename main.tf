8a9617c5f22ac7f1317bb44794c4f0a91bac0de1
Sayam Masood <mixpix3ls@protonmail.com>
2020-05-21 12:35:01 -0500

    Supplied subnet and az didn't match

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "bastion" {
   associate_public_ip_address = true
   # subnet_id = module.vpc-admin.private_subnets[0]
   subnet_id = "subnet-0325f8c9f62fec88d"
-  availability_zone = "us-west-1c"
+  availability_zone = "us-west-1b"
 }
 
 # Drupal Application TODOS
