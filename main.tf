4370401aa8b902f9e3415e5e5c648e572b303a3c
Sayam Masood <mixpix3ls@protonmail.com>
2020-05-21 07:33:30 -0500

    Adding more subnets

--- main.tf
+++ main.tf
@@ -12,4 +12,8 @@ module "vpc-app" {
   cidr = "192.169.0.0/16"
   azs             = [data.aws_availability_zones.azs-app.names[0], data.aws_availability_zones.azs-app.names[1]]
   private_subnets  = ["192.169.1.0/24", "192.169.2.0/24"]
+  public_subnets  = ["192.169.101.0/24", "192.169.102.0/24"]
+  database_subnets = ["192.169.201.0/24", "192.169.202.0/24"]
+  enable_nat_gateway = true
+  
 }
